0d2f38a88811bb78fe6917e31b7c143e83cf7d63
DeyanPavlin <d.iv.pavlin@gmail.com>
2024-09-02 13:06:49 +0300

    ButtonFixed

--- WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e86a78ada60bede7be15f32d5455c58a3659a718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f71a0f395f8836f39f3ba7c7d07268564e700bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
