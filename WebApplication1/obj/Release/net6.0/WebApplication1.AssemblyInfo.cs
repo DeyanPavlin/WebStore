bf95b660213d68fb68da542280f97940393dae44
DeyanPavlin <d.iv.pavlin@gmail.com>
2025-03-11 20:18:47 +0200

    Update

--- WebApplication1/obj/Release/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net6.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f760e99c69a75679eb6b75e956d556a33d0d139")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+938a932c58dacb8b3cfa76a08ce8c8520bfcc530")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
