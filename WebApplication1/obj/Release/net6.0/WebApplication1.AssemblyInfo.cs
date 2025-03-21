6792dab01a5179d11f07985e6dc9ec811061bfa6
DeyanPavlin <d.iv.pavlin@gmail.com>
2025-03-21 13:23:35 +0200

    gitignore fix

--- WebApplication1/obj/Release/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net6.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41d34e8aaeb0fde11cc006ebc6343e549c0f9d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8576dc6b5dccc30c8c2b938e258f833c0f7ef2c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
